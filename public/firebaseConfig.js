ce886061c20e60d2be953a2be1b37c5bb2f2909f
Kelly Ann Boodram <boodram.kellyann@gmail.com>
2024-04-14 17:37:12 +0000

    updates

---
+++ public/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAGxRizfMaBfh5YTT4ArWUnh4uEm1vxilE",
+    authDomain: "world-recipes-4210a.firebaseapp.com",
+    projectId: "world-recipes-4210a",
+    storageBucket: "world-recipes-4210a.appspot.com",
+    messagingSenderId: "319884363618",
+    appId: "1:319884363618:web:89b1c91ad0592569350e3b",
+    measurementId: "G-3KNBHZQF93"
+  };
+
+  export default firebaseConfig;
